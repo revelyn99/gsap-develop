cc1a626ea07c9a38e26bfb8e1e2391b6e3c714b4
Mark Braithwaite <mark.braithwaite@gmail.com>
2025-03-10 08:59:02 -0700

    .npmrc creation.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import gsap from 'gsap'
+// import DrawSVGPlugin from 'gsap/DrawSVGPlugin'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import './styles/style.css'
 
 gsap.registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(DrawSVGPlugin)
 
 var $logo = '.svg-logo'
 var $bg = '.background'
@@ -19,12 +21,14 @@ var $sec5 = '.sec-5'
 // // prettier-ignore
 // tl
 
+// gsap.from($sec2, { duration: 1, drawSVG: 0 })
+
 let tl = gsap.timeline({
   scrollTrigger: {
     trigger: $logo,
     scale: 1,
     start: 'center center',
-    end: '200%',
+    end: '550%',
     markers: true,
     pin: true,
     toggleActions: 'restart none none restart',
@@ -35,13 +39,17 @@ tl
   // prettier-ignore
 
   .to($logo, {
-    rotation: 360,
-    scale: 1.2,
-    ease: 'bounce.out',
+    // rotation: 360,
+    // scale: 1.2,
+    // ease: 'bounce.out',
   })
   .to($sec1, { opacity: 1 })
-  .to($sec2, { opacity: 1 })
+  .to($sec2, { opacity: 1, drawSVG: true })
   .to($sec3, { opacity: 0 })
   .to($sec4, { opacity: 0 })
   .to($sec5, { opacity: 0 })
   .to($bg, { opacity: 0 })
+  .to($sec3, { opacity: 1 })
+  .to($sec4, { opacity: 1 })
+  .to($sec5, { opacity: 1 })
+  .to($bg, { opacity: 1 })
