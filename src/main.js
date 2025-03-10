0fd64fbc7715d732332279b09e9d3e744789a399
Mark Braithwaite <mark.braithwaite@gmail.com>
2025-03-09 17:43:05 -0700

    Tween update.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import './styles/style.css'
 gsap.registerPlugin(ScrollTrigger)
 
 var $logo = '.svg-logo'
+var $bg = '.background'
+var $sec1 = '.sec-1'
+var $sec2 = '.sec-2'
+var $sec3 = '.sec-3'
+var $sec4 = '.sec-4'
+var $sec5 = '.sec-5'
 // var $scrollspace = '.scroll-space'
 
 // gsap.set($logo, { scale: 0.5 })
@@ -16,16 +22,26 @@ var $logo = '.svg-logo'
 let tl = gsap.timeline({
   scrollTrigger: {
     trigger: $logo,
+    scale: 1,
     start: 'center center',
     end: '200%',
     markers: true,
     pin: true,
-    toggleActions: 'restart none none pause',
+    toggleActions: 'restart none none restart',
   },
 })
 
-tl.to($logo, {
-  rotation: 360,
-  scale: 1,
-  ease: 'bounce.out',
-})
+tl
+  // prettier-ignore
+
+  .to($logo, {
+    rotation: 360,
+    scale: 1.2,
+    ease: 'bounce.out',
+  })
+  .to($sec1, { opacity: 1 })
+  .to($sec2, { opacity: 1 })
+  .to($sec3, { opacity: 0 })
+  .to($sec4, { opacity: 0 })
+  .to($sec5, { opacity: 0 })
+  .to($bg, { opacity: 0 })
